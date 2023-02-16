b43547af84de8540eb702fe53e6c6a1e411cdaab
Stephane0713 <stephaned2205@gmail.com>
2023-02-16 15:02:19 +0100

    deleted migrations

--- migrations/Version20230216140130.php
+++ migrations/Version20230216140130.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230216134052 extends AbstractMigration
+final class Version20230216140130 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -26,7 +26,6 @@ final class Version20230216134052 extends AbstractMigration
         $this->addSql('CREATE TABLE group_user (group_id INTEGER NOT NULL, user_id INTEGER NOT NULL, PRIMARY KEY(group_id, user_id), CONSTRAINT FK_A4C98D39FE54D947 FOREIGN KEY (group_id) REFERENCES "group" (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE, CONSTRAINT FK_A4C98D39A76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE)');
         $this->addSql('CREATE INDEX IDX_A4C98D39FE54D947 ON group_user (group_id)');
         $this->addSql('CREATE INDEX IDX_A4C98D39A76ED395 ON group_user (user_id)');
-        $this->addSql('CREATE TABLE members (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL)');
         $this->addSql('CREATE TABLE message (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, owner_id INTEGER NOT NULL, thread_id INTEGER NOT NULL, content CLOB NOT NULL, created_at DATETIME NOT NULL --(DC2Type:datetime_immutable)
         , updated_at DATETIME DEFAULT NULL, masked BOOLEAN NOT NULL, CONSTRAINT FK_B6BD307F7E3C61F9 FOREIGN KEY (owner_id) REFERENCES user (id) NOT DEFERRABLE INITIALLY IMMEDIATE, CONSTRAINT FK_B6BD307FE2904019 FOREIGN KEY (thread_id) REFERENCES thread (id) NOT DEFERRABLE INITIALLY IMMEDIATE)');
         $this->addSql('CREATE INDEX IDX_B6BD307F7E3C61F9 ON message (owner_id)');
@@ -35,6 +34,10 @@ final class Version20230216134052 extends AbstractMigration
         , CONSTRAINT FK_31204C8358D797EA FOREIGN KEY (related_group_id) REFERENCES "group" (id) NOT DEFERRABLE INITIALLY IMMEDIATE, CONSTRAINT FK_31204C837E3C61F9 FOREIGN KEY (owner_id) REFERENCES user (id) NOT DEFERRABLE INITIALLY IMMEDIATE)');
         $this->addSql('CREATE INDEX IDX_31204C8358D797EA ON thread (related_group_id)');
         $this->addSql('CREATE INDEX IDX_31204C837E3C61F9 ON thread (owner_id)');
+        $this->addSql('CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles CLOB NOT NULL --(DC2Type:json)
+        , password VARCHAR(255) NOT NULL, nickname VARCHAR(20) NOT NULL, is_deleted BOOLEAN NOT NULL, created_at DATETIME NOT NULL --(DC2Type:datetime_immutable)
+        )');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649E7927C74 ON user (email)');
     }
 
     public function down(Schema $schema): void
@@ -42,8 +45,8 @@ final class Version20230216134052 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE "group"');
         $this->addSql('DROP TABLE group_user');
-        $this->addSql('DROP TABLE members');
         $this->addSql('DROP TABLE message');
         $this->addSql('DROP TABLE thread');
+        $this->addSql('DROP TABLE user');
     }
 }
