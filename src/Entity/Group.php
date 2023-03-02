087a87e87fe51beecd588592512ee4d861b85783
Nicolas-B06 <nicolas.bernard@nextsourcia.com>
2023-03-02 12:33:18 +0100

    [Update] Add group context

--- src/Entity/Group.php
+++ src/Entity/Group.php
@@ -6,33 +6,57 @@ use ApiPlatform\Metadata\ApiResource;
 use App\Repository\GroupRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: GroupRepository::class)]
 #[ORM\Table(name: '`group`')]
-#[ApiResource]
+#[ApiResource(
+    operations: [
+        new GetCollection(),
+        new Get(),
+        new Post(),
+        new Patch(),
+        new Delete(),
+    ],
+    normalizationContext: ['groups' => ['group:read']],
+    denormalizationContext: ['groups' => ['group:write']],
+)]
 class Group
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['group:read'])]
     private ?int $id = null;
 
+
     #[ORM\Column(length: 255)]
+    #[Groups(['group:write', 'group:read'])]
     private ?string $name = null;
 
+
     #[ORM\Column(type: Types::TEXT)]
+    #[Groups(['group:write', 'group:read'])]
     private ?string $description = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['group:write', 'group:read'])]
     private ?string $picture = null;
 
     #[ORM\Column]
     private ?bool $isDeleted = null;
 
     #[ORM\Column]
+    #[Groups(['group:read'])]
     private ?\DateTimeImmutable $createdAt = null;
+
 
     #[ORM\ManyToOne(inversedBy: 'ownedGroups')]
     #[ORM\JoinColumn(nullable: false)]
